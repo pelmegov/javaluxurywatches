8fbaf177869ec6216bb572377496dcf39ecfb3bd
Stepan Pelmegov <pelmegovsa@gmail.com>
2017-05-16 22:07:57 +0300

    Fix tests.

--- src/test/java/ru/javaluxurywatches/controller/BaseControllerTest.java
+++ src/test/java/ru/javaluxurywatches/controller/BaseControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.javaluxurywatches.Application;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {Application.class})
 class BaseControllerTest {
