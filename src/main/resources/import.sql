7c78a1db5031c07cdf6b263c73bb2dd1c4045eef
Stepan Pelmegov <pelmegovsa@gmail.com>
2017-05-16 22:19:35 +0300

    Added fixtures for "user" table.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO "user" (login, password, first_name, last_name) VALUES ('admin', '123456', 'walter', 'white');
+INSERT INTO "user" (login, password, first_name, last_name) VALUES ('user', '123456', 'jesse', 'pinkman');
