f9f3590cfb5a5948d9303cb649b799decd122157
Pelmegov Stepan <modkomi@mail.ru>
2017-07-05 23:23:40 +0300

    Revert "add User, Product Services, change Product, User Controllers, add que…"

--- src/main/java/ru/javaluxurywatches/controller/UserController.java
+++ src/main/java/ru/javaluxurywatches/controller/UserController.java
@@ -5,32 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.javaluxurywatches.model.user.User;
 import ru.javaluxurywatches.repository.user.UserRepository;
-import ru.javaluxurywatches.services.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
-//    private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    private UserService userService;
-
-//    @Autowired
-//    public UserController(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @RequestMapping("/")
     public Iterable<User> users() {
-        return userService.getAll();
+        return userRepository.findAll();
     }
 
     boolean testAddUser() {
         User user = new User();
         user.setLogin("TestUser");
-        userService.saveUser(user);
-        String login = userService.getByLogin(user.getLogin()).getLogin();
+        userRepository.save(user);
+        String login = userRepository.findByLogin(user.getLogin()).getLogin();
         return login != null && login.equals(user.getLogin());
     }
 
--- src/main/java/ru/javaluxurywatches/repository/shop/CategoryRepository.java
+++ src/main/java/ru/javaluxurywatches/repository/shop/CategoryRepository.java
@@ -1,14 +1,10 @@
 package ru.javaluxurywatches.repository.shop;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.javaluxurywatches.model.shop.Category;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-
-    @Query("select c from Category c where c.link=:link")
-    Category findByLink(@Param("link") String link);
+    Category findByLink(String link);
 }
