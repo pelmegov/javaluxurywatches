925a88bdce19a1e2ac22309be729edb2317f59e4
Sanya <sanya0368@yandex.ru>
2017-07-30 19:24:37 +0300

    Add inerceptor for user

--- src/main/java/ru/javaluxurywatches/interceptor/AppInterceptor.java
+++ src/main/java/ru/javaluxurywatches/interceptor/AppInterceptor.java
@@ -1,15 +1,27 @@
 package ru.javaluxurywatches.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import ru.javaluxurywatches.model.user.User;
+import ru.javaluxurywatches.service.user.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class AppInterceptor extends HandlerInterceptorAdapter {
+
+    private UserService userService;
+
+    @Autowired
+    public AppInterceptor(UserService userService) {
+        this.userService = userService;
+    }
 
     @Value("${blog.page-size}")
     private Integer blogPageSize;
@@ -24,8 +36,13 @@ public class AppInterceptor extends HandlerInterceptorAdapter {
         if (modelAndView != null) {
             modelAndView.addObject("blogPageSize", blogPageSize);
             modelAndView.addObject("categoryPageSize", categoryPageSize);
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            User user = null;
+            if (auth.getPrincipal() != "anonymousUser") {
+                user = userService.findByLogin(auth.getName());
+            }
+            modelAndView.addObject("user", user);
             super.postHandle(request, response, handler, modelAndView);
         }
     }
-
 }
