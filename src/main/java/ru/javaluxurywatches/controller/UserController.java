9b982f6c28ec9ed9b728d8b7190acbcab41c7047
Alexander Ivanov <by.ivanovs@gmail.com>
2017-08-10 17:28:00 +0300

    Add update action for profile settings

--- src/main/java/ru/javaluxurywatches/controller/UserController.java
+++ src/main/java/ru/javaluxurywatches/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.javaluxurywatches.model.user.User;
+import ru.javaluxurywatches.model.user.UserDetail;
 import ru.javaluxurywatches.service.user.UserDetailService;
 import ru.javaluxurywatches.service.user.UserService;
 
@@ -45,15 +46,30 @@ public class UserController {
     public String mainSetting(@ModelAttribute User user,
                               final BindingResult result,
                               RedirectAttributes redirectAttrs) {
+        User currentUser = userService.findById(user.getId());
         if (result.hasErrors()) {
             return "redirect:profile/setting";
         }
-        userService.merge(user);
+        if (currentUser.getEmail().equals(user.getEmail())) {
+            redirectAttrs.addAttribute("", "");
+        }
+        userService.update(user);
         return "profile/main";
     }
 
     @RequestMapping(value = "/profile/account", method = RequestMethod.GET)
     public String accountSetting(Model model) {
+        return "profile/account";
+    }
+
+    @RequestMapping(value = "/profile/account", method = RequestMethod.POST)
+    public String doAccountSetting(@ModelAttribute UserDetail userDetail,
+                                   final BindingResult result,
+                                   RedirectAttributes redirectAttrs) {
+        if (result.hasErrors()) {
+            return "redirect:account";
+        }
+        userDetailService.update(userDetail);
         return "profile/account";
     }
 
