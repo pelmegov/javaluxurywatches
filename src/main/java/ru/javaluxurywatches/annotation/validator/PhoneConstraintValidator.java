8b3752e78f75a34f7e866c21a2f60f915a008016
pelmegov <pelmegovsa@gmail.com>
2017-08-20 08:41:05 +0300

    Fixed empty phone number 500 error (2).

--- src/main/java/ru/javaluxurywatches/annotation/validator/PhoneConstraintValidator.java
+++ src/main/java/ru/javaluxurywatches/annotation/validator/PhoneConstraintValidator.java
@@ -17,17 +17,17 @@ public class PhoneConstraintValidator implements ConstraintValidator<Phone, Stri
     @Override
     public boolean isValid(String phoneField, ConstraintValidatorContext cxt) {
 
-        if (nullable && phoneField == null) {
+        if (nullable && (phoneField == null || phoneField.isEmpty())) {
             return true;
         }
 
-        if (!nullable && phoneField == null) {
+        if (!nullable && (phoneField == null || phoneField.isEmpty())) {
             return false;
         }
 
         phoneField = phoneField.trim().replace("-", "");
 
-        if (!phoneField.isEmpty() && phoneField.substring(0, 1).equals("+")){
+        if (phoneField.substring(0, 1).equals("+")){
             return phoneField.matches("^\\+[0-9]{1}[0-9]{9,14}$");
         }
         return phoneField.matches("^[0-9]{9,11}$");
